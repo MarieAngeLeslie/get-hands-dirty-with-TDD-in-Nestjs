25c3272d5e623f0dfc568dbacb7cc9d0764b15ef
MarieAngeLeslie <42603506+MarieAngeLeslie@users.noreply.github.com>
2024-10-17 02:16:46 +0000

    feat: ensure appointment throw an error when end time is before start time

---
+++ appointment/appointment.service.spec.ts
@@ -0,0 +1,58 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppointmentService } from './appointment.service';
+
+// Accomplish the requirements to schedule an **Appointment,**
+/** 
+ * Les critères d'acceptance
+[x] An unconfirmed schedule should be created on success
+[x] The end time should not be before the start time
+[ ] The end time should be after the start time
+[ ] The patientId should be validated?
+ */
+
+describe('AppointmentService', () => {
+  let service: AppointmentService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [AppointmentService],
+    }).compile();
+
+    service = module.get<AppointmentService>(AppointmentService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should schedule an unconfirmed appointment for a user on success', () => {
+    const startTime = new Date('2024-10-17T14:00:00Z');
+    const endTime = new Date('2024-10-17T18:00:00Z');
+
+    const newAppointment = service.scheduleAppointment({
+      patientId: 1,
+      startTime,
+      endTime
+    });
+
+    expect(newAppointment).toEqual({
+      patientId: 1,
+      startTime,
+      endTime,
+      confirmed: false
+    })
+
+  })
+
+
+  it('should throw an error when end time is before start time', () => {
+    const startTime = new Date('2024-10-17T14:00:00Z');
+    const endTime = new Date('2024-10-17T13:00:00Z');
+
+    expect(() => service.scheduleAppointment({
+      patientId: 1,
+      startTime,
+      endTime
+    })).toThrow("appointment's endTime should be after startTime")
+  })
+});
---
+++ appointment/appointment.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+
+export interface Appointment {
+    patientId: number,
+    startTime: Date,
+    endTime: Date,
+    confirmed: boolean
+}
+
+export interface AppointmentInput {
+    patientId: number,
+    startTime: Date,
+    endTime: Date,
+}
+
+@Injectable()
+export class AppointmentService {
+    public scheduleAppointment(appointmentData: AppointmentInput): Appointment {
+        if (appointmentData.endTime < appointmentData.startTime) {
+            throw new Error("appointment's endTime should be after startTime")
+        }
+        return {
+            ...appointmentData,
+            confirmed: false
+        };
+    }
+}
